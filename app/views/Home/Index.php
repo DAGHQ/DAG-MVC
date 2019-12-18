377aefd3b96068a224acde30291344f152c1172d
DAG <kaseyber12@gmail.com>
2019-12-17 20:10:09 -0600

    Fixed the small stuff

---
+++ app/views/Home/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?=APP_NAME?> | Home</title>
+	<!--  
+		You dont need to get bootstrap its being pulled in by View::inc("header") Look at app/views/inc for more info
+	-->
+	<?php View::inc("header"); ?>
+</head>
+<body>
+	<?php View::inc("footer"); ?>
+</body>
+</html>
